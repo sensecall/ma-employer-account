e20c5eac71e2d3e5ca1264a071981c005c7c6ccf
Dan Sensecall <dan@sensecall.co.uk>
2019-07-02 11:52:28 +0100

    css

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,11 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/', (req, res) => {
+	res.render('index')
+})
+
+router.get('/account-home--1-0', (req, res) => {
+	res.render('_account-home')
+})
