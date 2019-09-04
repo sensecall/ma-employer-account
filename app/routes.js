aa63ee83b355c3943fc7fada885a81ab3103b909
Dan Sensecall <dan@sensecall.co.uk>
2019-09-04 20:29:50 +0100

    new

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
-
-module.exports = router
+// Get Sprint and Feature for URL links
+router.use('/', (req, res, next) => {
+  req.version = req.originalUrl.split('/')[1]
+  res.locals.version = req.version
+  next()
+})
 
 router.get('/', (req, res) => {
 	res.render('index')
@@ -12,3 +15,10 @@ router.get('/', (req, res) => {
 router.get('/account-home--1-0', (req, res) => {
 	res.render('account-home')
 })
+
+
+router.use(/\/interim-([0-9]+)-([0-9]+)/, (req, res, next) => {
+  require(`./views/interim-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
+})
+
+module.exports = router
