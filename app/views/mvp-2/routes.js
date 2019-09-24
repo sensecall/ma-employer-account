aae7d5f2a92a0eab0f670e91575d7e2c770bbb32
Dan Sensecall <dan@sensecall.co.uk>
2019-09-24 15:58:09 +0100

    v2 concept

---
+++ app/views/mvp-2/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+
+
+// Home page redirect
+router.get('/', (req, res) => {
+	res.redirect(`/${req.version}/account-home`)
+})
+
+router.post('/add-paye-question', (req, res) => {
+	if(req.session.data['add-paye-now'] == 'yes'){
+		res.redirect(`/${req.version}/sign-agreement-question`)
+	} else {
+		res.redirect(`/${req.version}/account-home`)
+	}
+})
+
+router.post('/sign-agreement-question', (req, res) => {
+	res.redirect(`/${req.version}/account-home`)
+})
+
+
+module.exports = router
