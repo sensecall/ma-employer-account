d075aa795923f45362f00ed8213958423d789e80
Dan Sensecall <dan@sensecall.co.uk>
2019-10-16 14:52:00 +0100

    transactional v2

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 
 // Get Sprint and Feature for URL links
 router.use('/', (req, res, next) => {
-  req.version = req.originalUrl.split('/')[1]
-  res.locals.version = req.version
-  next()
+	req.version = req.originalUrl.split('/')[1]
+	res.locals.version = req.version
+	next()
 })
 
 router.get('/', (req, res) => {
@@ -16,41 +16,45 @@ router.get('/account-home--1-0', (req, res) => {
 	res.render('account-home')
 })
 
+router.post('/config', (req, res) => {
+	let prototypeVersion = req.session.data['prototype-version']
+
+	res.redirect(prototypeVersion)
+})
+
 // STABLE URL FOR OTHER TEAMS TO POINT TO
 router.get('/stable', (req, res) => {
-	res.redirect('mvp-1-1/account-home')
+	let prototypeVersion = req.session.data['prototype-version'] || 'mvp-1-1'
+
+	res.redirect(prototypeVersion + '/account-home')
 })
 
 // URLS for other team projects
+// var isLocal = if(req.get('host') === 'localhost')
+
 // Training provider permissions
 const providerPermissionsURL = 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/provider-permissions/providers'
 router.get('/provider-permissions', (req, res) => {
 	res.redirect(providerPermissionsURL)
 })
- 
+
 // Add paye
 const payeURL = 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/ways-to-add-paye-scheme'
-router.get('/add-paye', (req, res) => {
-	res.redirect(payeURL)
-})
 
 // sign agreement
 const signAgreementURL = 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/agreement'
-router.get('/sign-agreement', (req, res) => {
-	res.redirect(signAgreementURL)
-})
 
 
 router.use(/\/interim-([0-9]+)-([0-9]+)/, (req, res, next) => {
-  require(`./views/interim-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
+	require(`./views/interim-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
 })
 
 router.use(/\/transactional-([0-9]+)-([0-9]+)/, (req, res, next) => {
-  require(`./views/transactional-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
+	require(`./views/transactional-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
 })
 
-router.use(/\/mvp-([0-9]+)/, (req, res, next) => {
-  require(`./views/mvp-${req.params[0]}/routes`)(req, res, next);
+router.use(/\/mvp-([0-9]+)-([0-9]+)/, (req, res, next) => {
+	require(`./views/mvp-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
 })
 
 module.exports = router
---
+++ app/views/mvp-1-2/routes.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+
+
+// Home page redirect
+router.get('/', (req, res) => {
+	res.redirect(`/${req.version}/account-home`)
+})
+
+router.post('/add-paye-question', (req, res) => {
+	if(req.session.data['add-paye-now'] == 'yes'){
+		res.redirect(`/${req.version}/sign-agreement-question`)
+	} else {
+		res.redirect(`/${req.version}/account-home`)
+	}
+})
+
+router.post('/sign-agreement-question', (req, res) => {
+	res.redirect(`/${req.version}/account-home`)
+})
+
+router.get('/add-paye', (req, res) => {
+	if( req.get('host').includes('localhost') ){
+		req.session.data['add-paye-now'] = 'yes'
+		
+		res.redirect(`/${req.version}/account-home`)
+	} else {
+		res.redirect(payeURL)
+	}
+})
+
+router.get('/sign-agreement', (req, res) => {
+	if( req.get('host').includes('localhost') ){
+		req.session.data['sign-agreement-now'] = 'yes'
+		
+		res.redirect(`/${req.version}/account-home`)
+	} else {
+		res.redirect(signAgreementURL)
+	}
+})
+
+
+module.exports = router
