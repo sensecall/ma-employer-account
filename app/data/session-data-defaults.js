702d29e181171b8be3113ab9b7e5d2bb411b72e5
Dan Sensecall <dan@sensecall.co.uk>
2019-10-17 12:26:07 +0100

    fix

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -19,9 +19,9 @@ Example usage:
 
 module.exports = {
 	// URLS
-	'providerPermissionsURL': 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/provider-permissions/providers'
-	'payeURL': 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/ways-to-add-paye-scheme'
-	'signAgreementURL': 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/agreement'
+	'providerPermissionsURL': 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/provider-permissions/providers',
+	'payeURL': 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/ways-to-add-paye-scheme',
+	'signAgreementURL': 'https://esfa:educ4tion@das-registration-prototype.herokuapp.com/agreement',
 
 	// Other
 	'employer-name': "Aardvark Zebra Ltd"
