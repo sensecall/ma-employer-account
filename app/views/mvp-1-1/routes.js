b961b387106964d5a76997097ebdf725432ef13b
Dan Sensecall <dan@sensecall.co.uk>
2019-10-17 14:57:16 +0100

    url

--- app/views/mvp-1-1/routes.js
+++ app/views/mvp-1-1/routes.js
@@ -7,6 +7,16 @@ router.get('/', (req, res) => {
 	res.redirect(`/${req.version}/account-home`)
 })
 
+// add paye external linking
+router.get('/add-paye', (req, res) => {
+	res.redirect(req.session.data['payeURL'] + '?referrer=' + res.locals.fullUrl + '/' + req.version + '/account-home')
+})
+
+// sign agreement external linking
+router.get('/sign-agreement', (req, res) => {
+	res.redirect(req.session.data['signAgreementURL'] + '?referrer=' + res.locals.fullUrl + '/' + req.version + '/account-home')
+})
+
 router.post('/add-paye-question', (req, res) => {
 	if(req.session.data['add-paye-now'] == 'yes'){
 		res.redirect(`/${req.version}/sign-agreement-question`)
